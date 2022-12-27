49723313c1f3a9a83fd070cd1d67ad934dc52462
thephotogenicbug <navinkumarachari@gmail.com>
2022-12-27 12:16:15 +0530

    import all the componets inside Router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './components/Home/Home';
+import Cart from './components/Cart/Cart';
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 }
