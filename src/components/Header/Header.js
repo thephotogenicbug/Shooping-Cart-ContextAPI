e9b71aa0717fc2ffee4035a499323dd324519ad1
thephotogenicbug <navinkumarachari@gmail.com>
2022-12-27 12:11:43 +0530

    Design Header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Badge, Container, Dropdown, FormControl, Nav, Navbar } from "react-bootstrap";
+import {AiOutlineShoppingCart} from 'react-icons/ai'
+
+const Header = () => {
+  return (
+    <Navbar bg="dark" variant="dark" style={{ height: 80 }}>
+      <Container>
+        <Navbar.Brand>
+          <a href="#">Shopping Cart</a>
+        </Navbar.Brand>
+        <Navbar.Text className="search">
+          <FormControl
+            style={{ width: 400 }}
+            placeholder="Search a product"
+            className="m-auto"
+          />
+        </Navbar.Text>
+        <Nav style={{marginRight: 200}}>
+          <Dropdown >
+            <Dropdown.Toggle variant="success" id="dropdown-basic">
+              <AiOutlineShoppingCart style={ { marginRight: 10 } } />
+              <Badge bg="transparent">10</Badge>
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu style={{ minWidth:370 }}>
+               <span style={{padding:10}}>Cart is Empty!</span>
+            </Dropdown.Menu>
+          </Dropdown>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Header;
